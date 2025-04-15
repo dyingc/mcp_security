00f217d8424e4ee94ae8272671055d8af5285838
ying.dong <ying.dong@oracle.com>
2025-04-15 15:41:42 -0700

    Enhanced finished

--- client/mcp_client.py
+++ client/mcp_client.py
@@ -1,7 +1,5 @@
 import asyncio
-import os
 import sys
-import yaml
 from pathlib import Path
 from dotenv import load_dotenv
 
@@ -18,7 +16,8 @@ if str(project_root) not in sys.path:
 from client.utils import (
     get_llm,
     get_mcp_client_config,
-    get_config
+    output_responses,
+    output_tools,
 )
 
 class DemoMCPClient:
@@ -29,7 +28,9 @@ class DemoMCPClient:
 
     async def run(self, query: str)->List[AnyMessage]:
         async with MultiServerMCPClient(self.mcp_config) as mcp_client:
-            self.agent = create_react_agent(self.llm, mcp_client.get_tools())
+            tools = mcp_client.get_tools()
+            output_tools(tools)
+            self.agent = create_react_agent(self.llm, tools)
             response = await self.agent.ainvoke({"messages": [query]})
         return response.get("messages", [])
 
@@ -41,8 +42,7 @@ async def main():
     demo_client = DemoMCPClient(llm_name, config_file)
     query = "What is the result of 2 + 2?"
     response_messages = await demo_client.run(query)
-    for msg in response_messages:
-        print(msg.content)
+    output_responses(response_messages)
 
 if __name__ == "__main__":
     asyncio.run(main())
--- client/utils.py
+++ client/utils.py
@@ -2,7 +2,14 @@ from langchain_deepseek import ChatDeepSeek
 import yaml
 import os
 from langchain_core.language_models.chat_models import BaseChatModel
-from typing import Dict, Any
+from typing import Dict, Any, List
+from langchain_core.messages import (
+    AnyMessage,
+    HumanMessage,
+    AIMessage,
+    ToolMessage,
+)
+from langchain_core.tools.structured import StructuredTool
 
 def get_llm(llm_name: str, config_file:str="client/client_config.yaml")->BaseChatModel:
     with open(config_file, "r") as file:
@@ -32,3 +39,54 @@ def get_config(config_file: str = "client/client_config.yaml"):
     with open(config_file, "r") as file:
         return yaml.safe_load(file)
 
+def output_responses(messages: List[AnyMessage]):
+    """
+    Prints out the content of each message in the given list.
+
+    Args:
+    messages (List[Message]): A list of messages where each message is an instance of either AIMessage, HumanMessage, or another type.
+    """
+    for msg in messages:
+        if isinstance(msg, AIMessage):
+            if msg.content:
+                print(f"\nAI: {msg.content}")
+            if msg.tool_calls:
+                print("\nAI calls tools:")
+                tool_calls = msg.tool_calls
+                for t in tool_calls:
+                    func_name = t.get('name', None)
+                    args = t.get('args', [])
+                    if func_name:
+                        args_repr = ", ".join([f"{k}='{v}'" if isinstance(v, str) else f"{k}={v}" for k, v in args.items()])
+                        func_call_repr = f"- Function call: {func_name}({args_repr})"
+                        print(func_call_repr)
+        elif isinstance(msg, HumanMessage):
+            print(f"\nUser: {msg.content}")
+        elif isinstance(msg, ToolMessage):
+            print(f"\nTool execution result: {msg.content}")
+        else:
+            print(msg.content)
+
+def output_tools(tools: List[StructuredTool]):
+    """
+    Prints out the name and description, as well as the info of arguments, of each tool in the given list.
+
+    Args:
+    tools (List[StructuredTool]): A list of tools where each tool is an instance of StructuredTool.
+    """
+    for i, tool in enumerate(tools):
+        tool_name = tool.name
+        tool_desc = tool.description
+        tool_args = tool.args_schema
+        print(f"Tool {i:02d}")
+        print(f"\tName: {tool_name}")
+        print(f"\tDescription:\n'''\n{tool_desc}\n'''")
+        print(f"\tArgs:")
+        properties = tool_args.get("properties", {})
+        required = tool_args.get("required", [])
+        for prop, schema in properties.items():
+            if prop in required:
+                print(f"\t\t{prop} (required): {schema}")
+            else:
+                print(f"\t\t{prop} (optional): {schema}")
+        print("\n")
